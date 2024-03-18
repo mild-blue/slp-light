c81de0dbabfda7494d8129f77cde47b2d9a8c7e1
Pavel Mach <machpavel@gmail.com>
2024-03-18 17:35:53 +0100

    Forgotten files removed.

---
+++ AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using slp.light.DTO;
+using slp.light.Model;
+
+namespace slp.light
+{
+    public class MapperConfig
+    {
+        public static Mapper InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserOutDto>()
+                    .ForMember(x => x.Roles, x => x.MapFrom(y => y.Roles.Select(r => r.ToString())));
+            });
+
+            var mapper = new Mapper(config);
+            return mapper;
+        }
+    }
+}
---
+++ DTO/UserOutDto.cs
@@ -0,0 +1,10 @@
+﻿namespace slp.light.DTO
+{
+    public class UserOutDto
+    {
+        public required string Username { get; set; }
+        public required string FullName { get; set; }
+        public required IEnumerable<string> Roles { get; set; }
+        public required bool IsActive { get; set; }
+    }
+}
